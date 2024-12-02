85ef6551d747e9635d407b5266299d20d5b4688c
Stephen Nicholas Swatman <stephen@v25.nl>
2024-12-02 09:56:26 +0100

    Solutions to day 2

--- src/Problems/Day02.hs
+++ src/Problems/Day02.hs
@@ -1,9 +1,30 @@
 module Problems.Day02 (solution) where
 
-import Common.Solution (Day, notImplemented)
+import Text.Parsec (sepEndBy1, newline, char)
+
+import Common.Parse (AocInput, aocParse, integer)
+import Common.Helper (pairwise)
+import Common.Solution (Day)
+
+input :: AocInput () [[Integer]]
+input = sepEndBy1 (sepEndBy1 integer (char ' ')) newline
+
+without :: Integer -> [Integer] -> [Integer]
+without i x = [q | (p, q) <- zip [0..] x, p /= i]
+
+withoutPermutations :: [Integer] -> [[Integer]]
+withoutPermutations x = [without (toInteger i) x | i <- [0..length x]] 
+
+isSafeBasic :: [Integer] -> Bool
+isSafeBasic x = ((all (>= 1) y) || (all (<= -1) y)) && (and [(abs i) <= 3 | i <- y])
+    where
+        y = map (uncurry (-)) . pairwise $ x
+
+isSafeComplex :: [Integer] -> Bool
+isSafeComplex x = (isSafeBasic x) || (any isSafeBasic (withoutPermutations x))
 
 solution :: Day
 solution = (
-        notImplemented,
-        notImplemented
+        show . length . filter isSafeBasic . aocParse input (),
+        show . length . filter isSafeComplex . aocParse input ()
     )
