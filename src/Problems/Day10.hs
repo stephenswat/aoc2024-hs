3938a1b84c4bf7e1553a72c6db953152ff10c080
Stephen Nicholas Swatman <stephen@v25.nl>
2024-12-10 12:30:06 +0100

    Solutions to day 10

--- src/Problems/Day10.hs
+++ src/Problems/Day10.hs
@@ -1,9 +1,25 @@
 module Problems.Day10 (solution) where
 
-import Common.Solution (Day, notImplemented)
+import Data.Map (lookup, keys)
+import Data.Set (fromList, size)
+
+import Common.Geometry (Grid2D, Point2D, readGrid2DWith, neighbours4)
+import Common.Solution (Day)
+
+allTrails :: Grid2D Integer -> Point2D -> Integer -> [Point2D]
+allTrails g p v
+    | Data.Map.lookup p g == (Just v) && v == 9 = [p]
+    | Data.Map.lookup p g == (Just v) = concat [allTrails g q (v + 1) | q <- neighbours4 p]
+    | otherwise = []
+
+solveA :: Grid2D Integer -> Int
+solveA g = sum [size . fromList $ allTrails g p 0 | p <- keys g]
+
+solveB :: Grid2D Integer -> Int
+solveB g = sum [length $ allTrails g p 0 | p <- keys g]
 
 solution :: Day
 solution = (
-        notImplemented,
-        notImplemented
+        show . solveA . readGrid2DWith (\x -> read [x]),
+        show . solveB . readGrid2DWith (\x -> read [x])
     )
