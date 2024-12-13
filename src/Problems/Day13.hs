54273af7e7381710b295f268bd231c1007f01694
Stephen Nicholas Swatman <stephen@v25.nl>
2024-12-13 11:44:33 +0100

    Solutions to day 13

--- src/Problems/Day13.hs
+++ src/Problems/Day13.hs
@@ -1,9 +1,68 @@
 module Problems.Day13 (solution) where
 
-import Common.Solution (Day, notImplemented)
+import Data.Maybe (catMaybes)
+import Text.Parsec (sepBy1, string, newline)
+
+import Common.Parse (AocInput, aocParse, integer)
+import Common.Solution (Day)
+
+type Matrix = (Integer, Integer, Integer, Integer, Integer, Integer)
+
+input :: AocInput () [Matrix]
+input = sepBy1 mat newline
+    where
+        mat = do
+            _ <- string "Button A: X+"
+            i1 <- integer
+            _ <- string ", Y+"
+            i2 <- integer
+            _ <- newline
+            _ <- string "Button B: X+"
+            i3 <- integer
+            _ <- string ", Y+"
+            i4 <- integer
+            _ <- newline
+            _ <- string "Prize: X="
+            i5 <- integer
+            _ <- string ", Y="
+            i6 <- integer
+            _ <- string "\n"
+            return (i1, i2, i3, i4, i5, i6)
+
+solveLinEq :: Matrix -> Maybe (Integer, Integer)
+solveLinEq (aax, aay, abx, aby, yx, yy)
+    | xm == 0 && ym == 0 = Just (x, y)
+    | otherwise = Nothing
+    where
+        lcm1 = lcm aax aay
+        aax1 = lcm1
+        abx1 = (lcm1 `div` aax) * abx
+        aby1 = (lcm1 `div` aay) * aby
+        yx1 = (lcm1 `div` aax) * yx
+        yy1 = (lcm1 `div` aay) * yy
+        aax2 = aax1
+        abx2 = abx1
+        yx2 = yx1
+        aby2 = aby1 - abx1
+        yy2 = yy1 - yx1
+        lcm2 = lcm abx2 aby2
+        aax3 = (lcm2 `div` abx2) * aax2
+        aby3 = lcm2
+        yx3 = (lcm2 `div` abx2) * yx2
+        yy3 = (lcm2 `div` aby2) * yy2
+        (x, xm) = (yx3 - yy3) `divMod` aax3
+        (y, ym) = yy3 `divMod` aby3
+
+cost :: (Integer, Integer) -> Integer
+cost (a, b)
+    | a < 0 || b < 0 = 0
+    | otherwise = 3 * a + b
+
+matModB :: Matrix -> Matrix
+matModB (aax, aay, abx, aby, yx, yy) = (aax, aay, abx, aby, yx + 10000000000000, yy + 10000000000000)
 
 solution :: Day
 solution = (
-        notImplemented,
-        notImplemented
+        show . sum . map cost . catMaybes . map solveLinEq . aocParse input (),
+        show . sum . map cost . catMaybes . map (solveLinEq . matModB) . aocParse input ()
     )
